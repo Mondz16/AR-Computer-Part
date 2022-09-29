d6c6c7021d868337a823c41dd424f1a381a612f4
Christian Mundal <70058199+Mondz16@users.noreply.github.com>
2022-09-29 11:31:21 +0800

    prototype update 3

--- AR Computer Part/Assets/Scripts/UIManager.cs
+++ AR Computer Part/Assets/Scripts/UIManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,9 +9,16 @@ public class UIManager : MonoBehaviour
 {
     public ComputerPartsCollection ComputerPartsCollection;
 
+    [Header("BG nad Header Colors")] 
+    public Color[] Colors;
+    
+    [Header("Main Devices")] 
+    public Transform SidePanelTransform;
+
     [Header("Sub Devices")] 
     public GameObject SubDevicePanel;
     public TMP_Text DeviceName;
+    public Image DeviceNameBG;
     public Transform SubDeviceHolder;
     public SubDevicesButton SubDeviceButton;
 
@@ -18,42 +26,28 @@ public class UIManager : MonoBehaviour
     public GameObject PartsPanel;
     public Image PartImage;
     public TMP_Text PartHeader;
+    public Image PartBG;
     public TMP_Text PartName;
     public Button DescriptionButton;
 
     public GameObject DescriptionPanel;
     public TMP_Text PartDescription;
 
-    public void OpenSystemUnitPanel()
+    public void OpenSideMenu()
     {
-        DeviceName.text = "System Unit";
-        foreach (Transform child in SubDeviceHolder) Object.Destroy(child.gameObject);
-
-        SubDevicePanel.SetActive(true);
-        for (int i = 0; i < ComputerPartsCollection.SystemUnit.Count; i++)
-        {
-            SubDevicesButton subDeviceButton = Instantiate(SubDeviceButton, SubDeviceHolder);
-            int temp = i;
-            subDeviceButton.SetDevice(ComputerPartsCollection.SystemUnit[temp].DeviceName , ComputerPartsCollection.SystemUnit[temp].DeviceImg , () => OpenPart(ComputerPartsCollection.SystemUnit[temp].Device));
-        }
+        SidePanelTransform.DOMoveX(540, .5f);
     }
-    public void OpenOutputDevicesPanel()
+    
+    public void CloseSideMenu()
     {
-        DeviceName.text = "Output Devices";
-        foreach (Transform child in SubDeviceHolder) Object.Destroy(child.gameObject);
-
-        SubDevicePanel.SetActive(true);
-        for (int i = 0; i < ComputerPartsCollection.OutputDevices.Count; i++)
-        {
-            SubDevicesButton subDeviceButton = Instantiate(SubDeviceButton, SubDeviceHolder);
-            int temp = i;
-            subDeviceButton.SetDevice(ComputerPartsCollection.OutputDevices[temp].DeviceName , ComputerPartsCollection.OutputDevices[temp].DeviceImg , () => OpenPart(ComputerPartsCollection.OutputDevices[temp].Device));
-        }
+        SidePanelTransform.DOMoveX(-540, .5f);
     }
     
     public void OpenInputDevicesPanel()
     {
         DeviceName.text = "Input Devices";
+        DeviceNameBG.color = Colors[0];
+        PartBG.color = Colors[0];
         foreach (Transform child in SubDeviceHolder) Object.Destroy(child.gameObject);
         
         SubDevicePanel.SetActive(true);
@@ -65,9 +59,27 @@ public class UIManager : MonoBehaviour
         }
     }
     
+    public void OpenSystemUnitPanel()
+    {
+        DeviceName.text = "System Unit";
+        DeviceNameBG.color = Colors[1];
+        PartBG.color = Colors[1];
+        foreach (Transform child in SubDeviceHolder) Object.Destroy(child.gameObject);
+
+        SubDevicePanel.SetActive(true);
+        for (int i = 0; i < ComputerPartsCollection.SystemUnit.Count; i++)
+        {
+            SubDevicesButton subDeviceButton = Instantiate(SubDeviceButton, SubDeviceHolder);
+            int temp = i;
+            subDeviceButton.SetDevice(ComputerPartsCollection.SystemUnit[temp].DeviceName , ComputerPartsCollection.SystemUnit[temp].DeviceImg , () => OpenPart(ComputerPartsCollection.SystemUnit[temp].Device));
+        }
+    }
+    
     public void OpenStorageDevicesPanel()
     {
         DeviceName.text = "Storage Devices";
+        DeviceNameBG.color = Colors[2];
+        PartBG.color = Colors[2];
         foreach (Transform child in SubDeviceHolder) Object.Destroy(child.gameObject);
         
         SubDevicePanel.SetActive(true);
@@ -78,7 +90,23 @@ public class UIManager : MonoBehaviour
             subDeviceButton.SetDevice(ComputerPartsCollection.StorageDevices[temp].DeviceName , ComputerPartsCollection.StorageDevices[temp].DeviceImg , () => OpenPart(ComputerPartsCollection.StorageDevices[temp].Device)); ;
         }
     }
+    
+    public void OpenOutputDevicesPanel()
+    {
+        DeviceName.text = "Output Devices";
+        DeviceNameBG.color = Colors[3];
+        PartBG.color = Colors[3];
+        foreach (Transform child in SubDeviceHolder) Object.Destroy(child.gameObject);
 
+        SubDevicePanel.SetActive(true);
+        for (int i = 0; i < ComputerPartsCollection.OutputDevices.Count; i++)
+        {
+            SubDevicesButton subDeviceButton = Instantiate(SubDeviceButton, SubDeviceHolder);
+            int temp = i;
+            subDeviceButton.SetDevice(ComputerPartsCollection.OutputDevices[temp].DeviceName , ComputerPartsCollection.OutputDevices[temp].DeviceImg , () => OpenPart(ComputerPartsCollection.OutputDevices[temp].Device));
+        }
+    }
+    
     private void OpenPart(Device device)
     {
         PartsPanel.SetActive(true);
